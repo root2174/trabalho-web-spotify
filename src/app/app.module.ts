8dae6557bfd25124377069d8470b27aaaf187863
Lucas Magalhaes <lucas.magalhaes@hp.com>
2020-03-12 15:00:06 -0300

    front-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { ShowcaseComponent } from './showcase/showcase.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, ShowcaseComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
